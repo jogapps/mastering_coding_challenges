2328840659a86140efc3a33097b2fd275d298672
jogapps <jogappsdeveloper@gmail.com>
2021-11-18 07:37:24 +0100

    working on recursive functions

--- 12. recursion/4. fibonacci.js
+++ 12. recursion/4. fibonacci.js
@@ -5,3 +5,27 @@ const fibonacci = (n) => {
 }
 
 console.log(fibonacci(6));
+
+
+const factorial = (num) => {
+    if(num === 1) return 1;
+    return value = num * factorial(num - 1);
+}
+
+const factorial2 = (num) => {
+    let value = 1;
+    for(let i = num; i <= num && i > 0; i--) {
+        value = value * i;
+    }
+    return value;
+}
+
+const factorial3 = (num) => {
+    let value = 1;
+    for(let i = 2; i <= num; i++) {
+        value = value * i;
+    }
+    return value;
+}
+
+console.log(factorial2(3));
--- 12. recursion/5. sum range.js
+++ 12. recursion/5. sum range.js
@@ -3,4 +3,27 @@ const sumRange = (num) => {
     return value = num + sumRange(num - 1);
 }
 
-console.log(sumRange(20));
+console.log(sumRange(3));
+
+const factorial = (num) => {
+    if(num === 1) return 1;
+    return value = num * factorial(num - 1);
+}
+
+const factorial2 = (num) => {
+    let value = 1;
+    for(let i = num; i <= num && i > 0; i--) {
+        value = value * i;
+    }
+    return value;
+}
+
+const factorial3 = (num) => {
+    let value = 1;
+    for(let i = 2; i <= num; i++) {
+        value = value * i;
+    }
+    return value;
+}
+
+console.log(factorial2(3));
